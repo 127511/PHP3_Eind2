abaa36dc5cdea8e1f4ee3ef25f0f3d4750fc010a
Charlotte <charlotte@pc.pc>
2020-08-20 14:00:22 +0200

    +css

--- classes/Field.php
+++ classes/Field.php
@@ -7,7 +7,7 @@ class Field{
     public function createField(){
 
 
-        for ($i=1; $i < 13; $i++) {
+        for ($i=1; $i < 17; $i++) {
             $button = new Card($i);
             $button->render($i);
             if ($i % 4 == 0) {

fbb93d1fa621486d4ae6d1272321f59d0de4819d
Jaimy Tieck <127511@student.drenthecollege.nl>
2020-08-26 12:23:14 +0200

    Made field scale based on different difficulties.

--- classes/Field.php
+++ classes/Field.php
@@ -3,10 +3,10 @@ require 'Card.php';
 
 
 class Field{
-
+    public $difficulty;
     public function createField(){
 
-
+      if($this->difficulty == 0){
         for ($i=1; $i < 17; $i++) {
             $button = new Card($i);
             $button->render($i);
@@ -14,6 +14,28 @@ class Field{
               echo "<br>";
             }
         }
+      }
+      else if($this->difficulty == 1){
+        for ($i=1; $i < 37; $i++) {
+            $button = new Card($i);
+            $button->render($i);
+            if ($i % 6 == 0) {
+              echo "<br>";
+            }
+        }
+      }
+      else if($this->difficulty == 2){
+        for ($i=1; $i < 65; $i++) {
+            $button = new Card($i);
+            $button->render($i);
+            if ($i % 8 == 0) {
+              echo "<br>";
+            }
+        }
+      }
+
+
+
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link rel="stylesheet" href="master.css">
     <meta charset="utf-8">
-    <title></title>
+    <title>Memory</title>
+    <link rel="stylesheet" href="master.css">
   </head>
   <body>
-    <?php
-        require 'classes/Field.php';
-        $field = new Field;
-        echo $field->createField();
-     ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="clickHandler.js" charset="utf-8"></script>
+    <h1>Memory</h1>
+    <h3>Select Difficulty</h3>
+    <form method="post">
+      <button name="0" class="diffButton">Easy</button> <br>
+      <button name="1" class="diffButton">Normal</button> <br>
+      <button name="2" class="diffButton">Hard</button> <br>
+    </form>
   </body>
 </html>
+<?php
+if (isset($_POST['0'])) {
+  echo "<script> location.href='play.php?diff=0'; </script>";
+}
+if (isset($_POST['1'])) {
+  echo "<script> location.href='play.php?diff=1'; </script>";
+}
+if (isset($_POST['2'])) {
+  echo "<script> location.href='play.php?diff=2'; </script>";
+}
+ ?>
